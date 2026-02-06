1309e11641521bb524c91207532f7035f5728512
Yasmin Alves <yasmin.alves16@outlook.com.br>
2026-02-05 21:20:37 -0300

    feat: add OrderItemDTO

--- src/main/java/com/deliverytech/delivery_api/dto/requests/CustomerOrderDTO.java
+++ src/main/java/com/deliverytech/delivery_api/dto/requests/CustomerOrderDTO.java
@@ -2,8 +2,6 @@ package com.deliverytech.delivery_api.dto.requests;
 
 import java.util.List;
 
-import com.deliverytech.delivery_api.model.OrderItem;
-
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,5 +22,5 @@ public class CustomerOrderDTO {
 
   @Valid
   @NotNull
-  private List<OrderItem> items;
+  private List<OrderItemDTO> items;
 }
