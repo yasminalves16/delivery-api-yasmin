b22df80f0659f85ba1f0bd7316b81f62f6f96a0b
Yasmin Alves <yasmin.alves16@outlook.com.br>
2026-02-06 22:37:28 -0300

    feat: add OpenAPI annotations to CustomerController

--- src/main/java/com/deliverytech/delivery_api/controller/CustomerController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.deliverytech.delivery_api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,38 +18,69 @@ import com.deliverytech.delivery_api.dto.requests.CustomerDTO;
 import com.deliverytech.delivery_api.dto.responses.CustomerResponseDTO;
 import com.deliverytech.delivery_api.service.CustomerService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/customers")
+@Tag(name = "Customer", description = "Endpoints para gerenciamento de clientes")
 public class CustomerController {
+  @Autowired
   private final CustomerService customerService;
 
   public CustomerController(CustomerService customerService) {
     this.customerService = customerService;
   }
 
+  @Operation(summary = "Registrar um novo cliente", description = "Cria um novo cliente com as informações fornecidas")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "201", description = "Cliente criado com sucesso"),
+      @ApiResponse(responseCode = "400", description = "Requisição inválida")
+  })
   @PostMapping
   public ResponseEntity<CustomerResponseDTO> registerCustomer(@Valid @RequestBody CustomerDTO customer) {
     return ResponseEntity.status(HttpStatus.CREATED).body(customerService.registerCustomer(customer));
   }
 
+  @Operation(summary = "Obter clientes ativos", description = "Retorna uma lista de clientes ativos")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Lista de clientes ativos retornada com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Nenhum cliente ativo encontrado")
+  })
   @GetMapping
   public List<CustomerResponseDTO> getActiveCustomers() {
     return customerService.getActiveCustomers();
   }
 
+  @Operation(summary = "Obter todos os clientes", description = "Retorna uma lista de todos os clientes, incluindo inativos")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Lista de clientes retornada com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Nenhum cliente encontrado")
+  })
   @GetMapping("/all")
   public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
     List<CustomerResponseDTO> allCustomers = customerService.getAllCustomers();
     return ResponseEntity.ok(allCustomers);
   }
 
+  @Operation(summary = "Buscar clientes por nome", description = "Retorna uma lista de clientes que correspondem ao nome fornecido")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Lista de clientes que correspondem ao nome retornada com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Nenhum cliente encontrado com o nome fornecido")
+  })
   @GetMapping("/search")
   public List<CustomerResponseDTO> searchCustomersByName(@RequestParam String name) {
     return customerService.searchCustomersByName(name);
   }
 
+  @Operation(summary = "Buscar cliente por ID", description = "Retorna um cliente que corresponde ao ID fornecido")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Cliente encontrado com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Cliente não encontrado com esse ID")
+  })
   @GetMapping("/{id}")
   public CustomerResponseDTO searchCustomerById(@PathVariable Long id) {
     return customerService.searchCustomerById(id);
@@ -59,7 +91,12 @@ public class CustomerController {
   // Customer updatedCustomer) {
   // return customerService.updateCustomer(id, updatedCustomer);
   // }
-
+  
+  @Operation(summary = "Ativar/Inativar cliente", description = "Altera o status de ativo do cliente, permitindo ativar ou inativar um cliente")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Status do cliente alterado com sucesso"),
+      @ApiResponse(responseCode = "404", description = "Cliente não encontrado com esse ID")
+  })
   @PatchMapping("/{id}/toggle")
   public CustomerResponseDTO toggleCustomerActive(@PathVariable Long id) {
     return customerService.toggleCustomerActive(id);
