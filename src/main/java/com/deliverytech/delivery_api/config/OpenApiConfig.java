25721ae05fe8f1d75d5d58d18013ddfa2e853600
Yasmin Alves <yasmin.alves16@outlook.com.br>
2026-02-09 19:11:49 -0300

    feat: add OpenAPI configuration for Delivery API documentation

--- src/main/java/com/deliverytech/delivery_api/config/OpenApiConfig.java
+++ src/main/java/com/deliverytech/delivery_api/config/OpenApiConfig.java
@@ -1,10 +1,14 @@
 package com.deliverytech.delivery_api.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,7 +19,11 @@ public class OpenApiConfig {
         .info(new Info()
             .title("Delivery API")
             .version("1.0")
-            .description("API para gerenciamento de entregas"));
+            .contact(new Contact().name("Yasmin").email("yasmin.alves16@outlook.com"))
+            .description("API para gerenciamento de entregas"))
+        .servers(List.of(new Server()
+            .url("http://localhost:8080")
+            .description("Servidor local")));
   }
 
 }
